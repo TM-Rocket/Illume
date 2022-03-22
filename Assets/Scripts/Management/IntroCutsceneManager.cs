58524253be08f22dfe0ea6257621c0cd1ba5f237
junekaa06 <junekaa06@gmail.com>
2022-03-21 23:38:56 -0700

    Implemented BGM for Beta Release

--- Assets/Scripts/Management/IntroCutsceneManager.cs
+++ Assets/Scripts/Management/IntroCutsceneManager.cs
@@ -4,20 +4,35 @@ using UnityEngine.Playables;
 public class IntroCutsceneManager : MonoBehaviour
 {
     private PlayableDirector _part1, _part2;
-    private bool _part1Played, _part2Played;
+    private bool _part1Played, _part2Played, _playAdventureBGM;
 
     void Start() {
         _part1 = GetComponent<PlayableDirector>();
         _part2 = GameObject.Find("IntroCutscenePart2").GetComponent<PlayableDirector>();
+        _playAdventureBGM = false;
+
     }
 
     void Update() {
         if(_part1.state == PlayState.Playing) {
+             if(_part1Played == false){
+                 FindObjectOfType<AudioManager>().Play("introBGM");
+             }
             _part1Played = true;
+
         }
 
         if(_part1.state != PlayState.Playing && _part1Played && !_part2Played) {
             _part2Played = true;
+        }
+
+        if(_part1Played == true && _part2Played == true)
+        {   
+            if(_playAdventureBGM == false){
+            FindObjectOfType<AudioManager>().Play("adventureBGM"); 
+            }
+            _playAdventureBGM = true;
+            
         }
     }
 }
