0ca9a01364d88231a798e3ad94322a079a810d0f
jake-pauls <jakepauls99@gmail.com>
2022-03-21 21:51:46 -0700

    Merged cave features into core scene and refined cutscene bugs

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,23 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 using Cinemachine;
 
 public class MainMenuManager : MonoBehaviour {
-    [SerializeField]
-    private GameObject _mainMenuUI;
+    [Header("Cinematics")]
     [SerializeField]
     private CinemachineVirtualCamera _mainMenuVCam;
     [SerializeField] 
     private CinemachineVirtualCamera _playerVCam;
-    [Header("Menu Buttons")]
     [SerializeField]
-    [Tooltip("Used to select the start button for gamepads when the menu loads")]
+    private PlayableDirector _introCutscene;
+    [SerializeField]
+    private List<GameObject> _cinematicUIElements;
+    [Header("Menu UI")]
+    [SerializeField]
+    private GameObject _mainMenuUI;
+    [SerializeField]
     private Button _startButton;
 
     [HideInInspector]
     public static bool IsMainMenuFocused = true;
 
     private void Awake() {
+        // Disable canvas' attached to cutescenes
+        foreach (GameObject uiElement in _cinematicUIElements) {
+            uiElement.SetActive(false);
+        }
+
         // Ensure priorities are setup properly
         _mainMenuVCam.Priority = 1;
         _playerVCam.Priority = 0;
@@ -33,5 +44,12 @@ public class MainMenuManager : MonoBehaviour {
         }
 
         IsMainMenuFocused = !IsMainMenuFocused;
+
+        // Re-enable canvas' attached to cutescenes
+        foreach (GameObject uiElement in _cinematicUIElements) {
+            uiElement.SetActive(true);
+        }
+
+        _introCutscene.Play();
     }
 }
